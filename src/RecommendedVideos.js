6e785e7658fd3704710eb86d69c76488f300e488
OrestDenysiuk <77453074+OrestDenysiuk@users.noreply.github.com>
2021-01-29 02:32:39 +0200

    Youtube

---
+++ src/RecommendedVideos.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export const RecomendedVideos = () => {
+    return (
+        <div>
+            <h2>
+                Recomended
+            </h2>
+        </div>
+    );
+};
+
